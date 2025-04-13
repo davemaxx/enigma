f1f96706f9a9a3d14982d8cbc1fa0b926ea0ab56
Davide <50943229+davemaxx@users.noreply.github.com>
2025-04-13 10:04:31 +0200

    Aggiunti form html e collegamento alla classe Rotore

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Criptazione Enigma</title>
+</head>
+<body>
+
+<a href="criptalettera.php">Cripta una lettera</a>
+<a href="criptamessaggiio.php">Cripta un messaggio</a>
+</body>
+</html>
--- src/rotore.php
+++ src/rotore.php
@@ -5,27 +5,49 @@ class Rotore
     private $mappatura;
     private $posizione;
 
-
-public function __construct($mappatura, $posizione = 0)
-{
-    $this->mappatura = $mappatura;
-    $this->posizione = $posizione;
-}
-
-public function codifica($lettera)
-{
-    // to do: codifica la lettera usando la mappatura;
-}
-
-public function ruota()
+    // Costruttore con parametro $mappatura e $posizione, quest'ultimo inizializzato a 0
+    public function __construct($mappatura, $posizione = 0)
     {
-        // TODO: aumenta la posizione e aggiorna la mappatura
+        $this->mappatura = $mappatura;
+        $this->posizione = $posizione;
     }
 
-public function getPosizione()
-    {
-        return $this->posizione;
-    }
+    public function ruota()
+        {
+            // TODO: aumenta la posizione e aggiorna la mappatura
+            $this->posizione = ($this->posizione + 1) % 26;
+        }
+
+    public function getPosizione()
+        {
+            return $this->posizione;
+        }
+
+    public function codifica($lettera)
+        {
+        // Applica la posizione attuale alla lettera
+        $alfabeto = range('A', 'Z');
+
+        //Trova l'indice della lettera originale
+        $indice = array_search($lettera, $alfabeto);
+        // Aggiungo un controllo    
+        if ($indice === false) {
+                echo "Lettera non presente. Usa solo lettere maiuscole dalla A alla Z";
+            }
+
+        //Calcola la lettera dopo lo scorrimento del rotore
+        $indiceCifrato = ($indice + $this->posizione) % 26;
+
+        //Trova la lettera mappata
+        $letteraScelta = $alfabeto[$indiceCifrato];
+
+        //Applica la mappatura
+        if (isset($this->mappatura[$letteraScelta]))
+        {
+            return $this->mappatura[$letteraScelta];
+        } else {
+            return $lettera; // se non è mappata restituisce la lettera così com'è
+        }     
 }
 
 ?>
