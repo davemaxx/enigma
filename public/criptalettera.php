6bd81c342735307bb4d5116c25e988a4356dca25
Davide <50943229+davemaxx@users.noreply.github.com>
2025-04-22 16:59:05 +0200

    Aggiunto il secondo rotore e visualizzazione degli steps

--- public/criptalettera.php
+++ public/criptalettera.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 session_start();
 ?>
 
@@ -17,6 +20,7 @@ session_start();
 </div>
 </div>
 
+
 <?php
 
 if (isset($_POST['lettera'])) {
@@ -26,7 +30,7 @@ $letteraInserita = $_POST['lettera'];
 $letteraInserita = strtoupper($letteraInserita);
 
 require_once '../src/rotore.php';
-$mappatura = [
+$mappatura1 = [
     'A' => 'D',
     'B' => 'F',
     'C' => 'Z',
@@ -66,25 +70,85 @@ $mappatura = [
     '9' => '5'
 ];
 
+$mappatura2 = [
+    'A' => 'I',
+    'B' => 'F',
+    'C' => 'Z',
+    'D' => 'Q',
+    'E' => 'M',
+    'F' => 'K',
+    'G' => 'R',
+    'H' => 'S',
+    'I' => 'O',
+    'J' => 'N',
+    'K' => 'B',
+    'L' => 'W',
+    'M' => 'H',
+    'N' => 'U',
+    'O' => 'P',
+    'P' => 'V',
+    'Q' => 'E',
+    'R' => 'C',
+    'S' => 'A',
+    'T' => 'Y',
+    'U' => 'G',
+    'V' => 'J',
+    'W' => 'T',
+    'X' => 'L',
+    'Y' => 'X',
+    'Z' => 'D',
+
+    '0' => '1',
+    '1' => '7',
+    '2' => '4',
+    '3' => '9',
+    '4' => '2',
+    '5' => '0',
+    '6' => '8',
+    '7' => '3',
+    '8' => '5',
+    '9' => '6'
+];
+
 
 // Creo un nuovo oggetto di tipo Rotore e gli passo il parametro $mappatura
+$posizione1 = isset($_SESSION['posizione1']) ? $_SESSION['posizione1'] : 0;
+$posizione2 = isset($_SESSION['posizione2']) ? $_SESSION['posizione2'] : 5;
 
-$posizione = isset($SESSION['posizione']) ? $_SESSION['posizione'] : 0;
-$rotore = new Rotore($mappatura, $posizione);
+// Creo un nuovo oggetto di tipo Rotore e gli passo il parametro $mappatura
+$rotore1 = new Rotore($mappatura1, $posizione1);
+$rotore2 = new Rotore($mappatura2, $posizione2);
 
-$risultato = $rotore->codifica($letteraInserita);
-$rotore->ruota();
+// Codifico la prima lettera nel rotore 1 e la metto nella variabile intermedia $parziale
+$primocambio = $rotore1->codifica($letteraInserita);
+$secondocambio = $rotore2->codifica($primocambio);
+
+
+$rotore1->ruota();
+// Ruoto il rotore 2 solo quando il primo ha fatto un giro completo.
+if ($rotore1->getPosizione() === 0) {
+    $rotore2->ruota();
+}
 
 // Aggiorno la variabile di sessione posizione da rotore
-$_SESSION['posizione'] = $rotore->getPosizione();
-
+$_SESSION['posizione1'] = $rotore1->getPosizione();
+$_SESSION['posizione2'] = $rotore2->getPosizione();
 }
 ?>
 
+<?php if (isset($letteraInserita)): ?>
+    <div class="step-enigma">
+        <p><strong>Lettera inserita:</strong> <?php echo $letteraInserita; ?></p>
+        <p><strong>Dopo il primo rotore:</strong> <?php echo $primocambio; ?></p>
+        <p><strong>Lettera cifrata finale:</strong> <?php echo $secondocambio; ?></p>
+        <p><strong>Posizione rotore 1:</strong> <?php echo $_SESSION['posizione1']; ?></p>
+        <p><strong>Posizione rotore 2:</strong> <?php echo $_SESSION['posizione2']; ?></p>
+    </div>
+<?php endif; ?>
 
-<?php if (!empty($risultato)): ?>
+<?php if (!empty($secondocambio)): ?>
     <div class="risultato">
-        Carattere cifrato: <strong><?php echo $risultato; ?></strong>
+        <p>Carattere cifrato: <strong><?php echo $secondocambio; ?></strong></p>
     </div>
 <?php endif; ?>
 
--- src/rotore.php
+++ src/rotore.php
@@ -16,6 +16,26 @@ class Rotore {
         $lettere = range('A', 'Z');
         $numeri = range('0', '9');
         $caratteri = array_merge($lettere, $numeri);
+        $indice = array_search($carattere, $caratteri);
+        if ($indice === false) {
+            echo "Carattere non valido.";
+            return null;
+        }
+        $indiceCifrato = ($indice + $this->posizione) % count($caratteri);
+        $letteraDopoRotore1 = $caratteri[$indiceCifrato];
+        if (isset($this->mappatura[$letteraDopoRotore1])) {
+            return $this->mappatura[$letteraDopoRotore1];
+        } else {
+            return $letteraDopoRotore1;
+        }
+    }
+
+    public function ruota() {
+        $this->posizione = ($this->posizione + 1) % 36;
+    }
+
+    public function getPosizione() {
+        return $this->posizione;
     }
 
 }
